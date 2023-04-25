2ec571298a5a57a8909e9fabec4e67b6914b09ba
Edgarv16 <edgv16@hotmail.com>
2023-04-25 05:16:12 -0500

    LOGIN

--- EanrrangoS2_Tarea2/Notas.xaml.cs
+++ EanrrangoS2_Tarea2/Notas.xaml.cs
@@ -34,56 +34,76 @@ namespace EanrrangoS2_Tarea2
            // DisplayAlert("Felicitacidades..", "Curso Aprobado " + aprobacion, "Cerrar");
 
         }
+        double notaSeg;
+        double notaExa1;
+        double notaPar1;
 
         private void btnCalcParcial1_Clicked(object sender, EventArgs e)
         {
-            double notaSeg = Convert.ToDouble(txtSegNota1.Text);
-            double notaExa1 = Convert.ToDouble(txtExam1.Text);
+            notaSeg = Convert.ToDouble(txtSegNota1.Text);
+            notaExa1 = Convert.ToDouble(txtExam1.Text);
+             //notaPar1 = Convert.ToDouble(txtParcial1.Text);
 
 
             // double suma = notaSeg + notaExa1;
-            if (txtSegNota1.Text != null || txtExam1.Text != null)
-            {
 
-               
-
-
-                if (notaSeg > 0 && notaSeg <= 10)
+         if (!string.IsNullOrEmpty(txtSegNota1.Text) && !string.IsNullOrEmpty(txtExam1.Text))
+          {
+                if (txtSegNota1.Text != null && txtExam1.Text != null)
                 {
-                     if (notaExa1 > 0 && notaExa1 <= 10)
+
+              
+                    if (notaSeg > 0 && notaSeg <= 10)
                     {
-                    double suma = notaSeg * 0.3 + notaExa1 * 0.2;
+                         if (notaExa1 > 0 && notaExa1 <= 10)
+                        {
+                        double suma = notaSeg * 0.3 + notaExa1 * 0.2;
 
-                    txtParcial1.Text = suma.ToString();
+                        txtParcial1.Text = suma.ToString();
+                        }
+                        else
+                         {
+                                DisplayAlert("Error..", "La Nota del Examne 1 debe estar entre: 0.1 - 10", "cerrar");
+                          }
+
                     }
-                    else
-                     {
-                            DisplayAlert("Error..", "La Nota del Examne 1 debe estar entre: 0.1 - 10", "cerrar");
-                      }
-
-                 }
                   else
                     {
                         DisplayAlert("Error..", "La Nota de Seguimiento 1 debe estar entre: 0.1 - 10", "cerrar");
 
                     }
             }
-          else
-            validaNotaSeg1();
+
+
+
+          }
+         else
+            {
+                DisplayAlert("Alerta", "Los campos de se encuentran vacios ", "cerrar");
+
+            }
+
+
 
         }
+
+        double notaSeg2;
+        double notaExa2;
 
         private void btnCalcParcial2_Clicked(object sender, EventArgs e)
         {
 
-            double notaSeg2 = Convert.ToDouble(txtSegNota2.Text);
-            double notaExa2 = Convert.ToDouble(txtExam2.Text);
+             notaSeg2 = Convert.ToDouble(txtSegNota2.Text);
+             notaExa2 = Convert.ToDouble(txtExam2.Text);
 
             //double suma2 = notaSeg2 + notaExa2;
 
             //txtParcial2.Text = suma2.ToString();
-            if (txtSegNota1.Text != null || txtExam1.Text != null)
+            if (!string.IsNullOrEmpty(txtSegNota2.Text) && !string.IsNullOrEmpty(txtExam2.Text))
             {
+
+                if (txtSegNota2.Text != null && txtExam2.Text != null)
+               {
 
                         if (notaSeg2 > 0 && notaSeg2 <= 10)
                         {
@@ -104,15 +124,24 @@ namespace EanrrangoS2_Tarea2
                             DisplayAlert("Error..", "La Nota de Seguimiento 2 debe estar entre: 0.1 - 10", "cerrar");
 
                         }
+                }
+           
             }
             else
-            validaNotaSeg2();
+            {
+                DisplayAlert("Alerta", "Los campos de se encuentran vacios ", "cerrar");
+            }
 
         }
 
+        double aprob;
+
         private void btnAprobado_Clicked(object sender, EventArgs e)
         {
-            double aprob = Convert.ToDouble(txtParcial1.Text) + Convert.ToDouble(txtParcial2.Text);
+
+          if (!string.IsNullOrEmpty(txtParcial1.Text) && !string.IsNullOrEmpty(txtParcial2.Text))
+          {
+                aprob = Convert.ToDouble(txtParcial1.Text) + Convert.ToDouble(txtParcial2.Text);
 
             if (aprob >= 7)
             {
@@ -129,7 +158,11 @@ namespace EanrrangoS2_Tarea2
               {
                  DisplayAlert("Failed ..", "Semestre Reprobado .. " + aprob, "Cerrar");
               }
-             
+          }
+          else
+            {
+                DisplayAlert("Alerta", "Los campos de se encuentran vacios ", "cerrar");
+            }
         }
 
         private void and(bool v)
@@ -147,32 +180,6 @@ namespace EanrrangoS2_Tarea2
            txtParcial2.Text = "";
         }
 
-
-        public void validaNotaSeg1()
-        {
-            if (!string.IsNullOrEmpty(txtSegNota1.Text) || !string.IsNullOrEmpty(txtExam1.Text))
-            {
-                validaNotaSeg1();
-
-            }
-            else
-            {
-                DisplayAlert("Alerta", "Los campos de se encuentran vacios ", "cerrar");
-            }
-        }
-
-        public void validaNotaSeg2()
-        {
-            if (!string.IsNullOrEmpty(txtSegNota2.Text) || !string.IsNullOrEmpty(txtExam2.Text))
-            {
-                validaNotaSeg2();
-
-            }
-            else
-            {
-                DisplayAlert("Alerta", "Los campos de se encuentran vacios ", "cerrar");
-            }
-        }
 
     }
 }
